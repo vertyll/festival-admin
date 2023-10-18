2e2f48c3e786a30ac098aa7e3a39911a646cfa4a
Mikołaj <gawrmiko@gmail.com>
2023-10-18 20:48:24 +0200

    dodanie działającego logowania za pomocą google

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,3 +1,5 @@
+import clientPromise from "@/lib/mongodb"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
@@ -8,6 +10,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 }
 
 export default NextAuth(authOptions)
