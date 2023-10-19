357e5d81b41084a461182443e5bc01eba2416719
Mikołaj <gawrmiko@gmail.com>
2023-10-19 09:03:47 +0200

    poprawiony sidebar i kolory

---
+++ components/Layout.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Sidebar from "@/components/Sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-indigo-600 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-5 rounded-3xl px-4"
+            onClick={() => signIn("google")}
+          >
+            Zaloguj się za pomocą <b>Google</b>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-indigo-600 min-h-screen flex">
+      <Sidebar />
+      <div className="bg-white flex-grow">{children}</div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useSession, signIn, signOut } from "next-auth/react"
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className='bg-blue-400 w-screen h-screen flex items-center'>
-        <div className='text-center w-full'>
-          <button className='bg-white p-5 rounded-3xl px-4' onClick={() => signIn('google')}>Zaloguj się za pomocą <b>Google</b></button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div>Zalogowano jako {session.user.email}</div>
-  );
+  return <Layout>
+    test
+  </Layout>
 }
