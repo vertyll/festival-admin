9ee3124a389f74207d04349daee3fac9077c6476
Mikołaj <gawrmiko@gmail.com>
2023-10-24 14:34:05 +0200

    poprawa w czytelności kodu

--- models/Order.jsx
+++ models/Order.jsx
@@ -2,15 +2,15 @@ import { model, models, Schema } from "mongoose";
 
 const OrderSchema = new Schema(
   {
-    line_items: {type: Object},
-    name: {type: String},
-    surname: {type: String},
-    email: {type: String},
-    city: {type: String},
-    postalCode: {type: String},
-    streetAddress: {type: String},
-    country: {type: String},
-    paid: {type: Boolean},
+    line_items: { type: Object },
+    name: { type: String },
+    surname: { type: String },
+    email: { type: String },
+    city: { type: String },
+    postalCode: { type: String },
+    streetAddress: { type: String },
+    country: { type: String },
+    paid: { type: Boolean },
   },
   {
     timestamps: true,
