a50344d51c1386c75e8d410e08754c1501cc5f1f
Mikołaj <gawrmiko@gmail.com>
2023-11-23 10:31:53 +0100

    dodanie informacji o autorze i opis projektu

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,6 +10,11 @@ export default function App({
     <SessionProvider session={session}>
       <Head>
         <title>Sunset Festival - administracja</title>
+        <meta name="author" content="Mikołaj Gawron" />
+        <meta
+          name="description"
+          content="Praca inżynierska - strona administracyjna festiwalu muzycznego z sklepem internetowym"
+        />
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
