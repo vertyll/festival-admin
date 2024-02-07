671e6e0a76ce3fdd5234bbaeca31a7aa63990c7d
Mikołaj <gawrmiko@gmail.com>
2024-02-07 08:37:12 +0100

    na potrzeby prezentacji uzytkownik nie musi byc adminem

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -5,7 +5,8 @@ import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 async function isAdminEmail(email) {
-  return !!(await Admin.findOne({ email }));
+  // return !!(await Admin.findOne({ email }));
+  return true;
 }
 
 export const authOptions = {
@@ -17,24 +18,24 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: async ({ session, token, user }) => {
-      if (await isAdminEmail(session?.user?.email)) {
-        return session;
-      } else {
-        return false;
-      }
-    },
-  },
+  // callbacks: {
+  //   session: async ({ session, token, user }) => {
+  //     if (await isAdminEmail(session?.user?.email)) {
+  //       return session;
+  //     } else {
+  //       return false;
+  //     }
+  //   },
+  // },
 };
 
 export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  if (!(await isAdminEmail(session?.user?.email))) {
-    res.status(401);
-    res.end();
-    throw "not an admin";
-  }
+  // if (!(await isAdminEmail(session?.user?.email))) {
+  //   res.status(401);
+  //   res.end();
+  //   throw "not an admin";
+  // }
 }
