7d0f8a072ea657a176f992856b97347d0f9e025c
Mikołaj <gawrmiko@gmail.com>
2024-02-07 08:42:22 +0100

    odkomentowano czesc kodu

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -18,24 +18,24 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  // callbacks: {
-  //   session: async ({ session, token, user }) => {
-  //     if (await isAdminEmail(session?.user?.email)) {
-  //       return session;
-  //     } else {
-  //       return false;
-  //     }
-  //   },
-  // },
+  callbacks: {
+    session: async ({ session, token, user }) => {
+      if (await isAdminEmail(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  // if (!(await isAdminEmail(session?.user?.email))) {
-  //   res.status(401);
-  //   res.end();
-  //   throw "not an admin";
-  // }
+  if (!(await isAdminEmail(session?.user?.email))) {
+    res.status(401);
+    res.end();
+    throw "not an admin";
+  }
 }
