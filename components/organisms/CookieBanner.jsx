0ec2458fdf2289020f635a2eb18d50e07a95e0f1
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-30 16:45:40 +0200

    feat: change cookies storage
    localStorage -> cookies

--- components/organisms/CookieBanner.jsx
+++ components/organisms/CookieBanner.jsx
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from "react";
 
+const setCookie = (name, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value};${expires};path=/`;
+};
+
+const getCookie = (name) => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
 const CookieBanner = () => {
   const [showBanner, setShowBanner] = useState(false);
 
   useEffect(() => {
-    const cookiesAccepted = localStorage.getItem("cookies_accepted");
+    const cookiesAccepted = getCookie("cookies_accepted");
     if (!cookiesAccepted) {
       setShowBanner(true);
     }
   }, []);
 
   const acceptCookies = () => {
-    localStorage.setItem("cookies_accepted", "true");
+    setCookie("cookies_accepted", "true", 365);
     setShowBanner(false);
   };
 
